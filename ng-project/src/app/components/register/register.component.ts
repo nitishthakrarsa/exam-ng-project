2035091bb8c1f0eb391f335f7d5a066d5fd4fe84
nitish.thakarar.sa <nitish.thakrar@solutionanalysts.com>
2017-07-12 12:40:11 +0530

    Final Push.

--- ng-project/src/app/components/register/register.component.ts
+++ ng-project/src/app/components/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { WebServiceService } from '../../services/web-service.service';
+import { ToasterContainerComponent, ToasterService } from 'angular2-toaster';
+import { FormBuilder, FormGroup, Validators, FormControl } from '@angular/forms';
+import { ValidationService } from "app/services/validation.service";
 
 @Component({
   selector: 'app-register',
@@ -7,7 +12,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  public formGrp: FormGroup;
+
+	constructor(private router: Router, private webService: WebServiceService, public toasterService: ToasterService, private formBuilder: FormBuilder) {
+		let self = this;
+		self.formGrp = self.formBuilder.group({
+			email: ['', [Validators.required, ValidationService.emailValidator]],
+			password: ['', Validators.required],
+      firstname:['', [Validators.required, Validators.maxLength(50)]]
+		});
+	}
 
   ngOnInit() {
   }
